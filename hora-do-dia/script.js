189eaac7769c7cfb20eb7362caa5aa0e7ce0af20
Tiago Ferreira <tiagoferreira7373@gmail.com>
2026-07-21 20:24:19 -0300

    Update script.js

--- hora-do-dia/script.js
+++ hora-do-dia/script.js
@@ -1,10 +1,16 @@
 function carregar() {
+    function atualiarRelogio() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
+    var min = data.getMinutes()
+
+    var minFormatado = min < 10 ? '0' + min : min;
     
-    msg.innerHTML = `Agora são ${hora} horas.`
+    msg.innerHTML = `Agora são ${hora}:${minFormatado} horas.`;
+
+    
 
     if (hora >= 0 && hora < 12) {
         // BOM DIA!
@@ -20,3 +26,9 @@ function carregar() {
         document.body.style.background = '#515154'
     }
 }
+
+ atualiarRelogio();
+
+    setInterval(atualiarRelogio, 1000);
+}
+
